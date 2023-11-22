942331332de5f81add125554dea6c07b0c57d15d
“TonyLu12138” <tony.lu@feixitek.com>
2023-11-22 06:38:17 +0000

    调整了change_corosync2_conf方法，补充了corosync.conf文件缺失部分

--- corosync_cmds.py
+++ corosync_cmds.py
@@ -1,10 +1,9 @@
 import utils
 
 corosync_conf_path = '/etc/corosync/corosync.conf'
-original_attr = {'cluster_name': 'cluster20231120',
-                 'bindnetaddr': '192.168.182.0'}
-interface_pos = '''                ttl: 1
-    }'''
+original_attr = {'cluster_name': 'debian',
+                 'bindnetaddr': '127.0.0.1'}
+interface_pos = 'crypto_hash: none'
 nodelist_pos = "logging {"
 quorum_pos = "        provider: corosync_votequorum"
 
@@ -43,43 +42,30 @@ def sync_time(ssh_conn=None):
 
 
 def change_corosync2_conf(cluster_name, bindnetaddr, bindnetaddr_list, interface, nodelist, ssh_conn=None):
-    editor = utils.FileEdit(corosync_conf_path)
-
-    if 'nodelist {' in editor.data:
-        editor.replace_data(f"cluster_name: {original_attr['cluster_name']}", f"cluster_name: {cluster_name}")
-        editor.replace_data(f"bindnetaddr: {original_attr['bindnetaddr']}", f"bindnetaddr: {bindnetaddr}")
-        editor.insert_data(interface, anchor=interface_pos, type='under')
-        editor.insert_data(nodelist, anchor=nodelist_pos, type='above')
-        if len(bindnetaddr_list) > 1:
-            editor.insert_data(f'\trrp_mode: passive', anchor='        # also set rrp_mode.', type='under')
-
-        utils.exec_cmd(f'echo "{editor.data}" > {corosync_conf_path}', ssh_conn)
-    else:
-        # Add new nodelist
-        editor.insert_data(nodelist, anchor=nodelist_pos, type='above')
-
-
-def change_corosync3_conf(cluster_name, nodelist, ssh_conn=None):
-    interface_content = '''
-        interface {
-            ringnumber: 0
-            bindnetaddr: 192.168.182.0
-            mcastport: 5405
-            ttl: 1
-        }
-    '''
     logging_content = '''
         syslog_facility: daemon
         timestamp: on
     '''
     quorum_content = '        expected_votes: 2'
-
-    editor = utils.FileEdit(corosync_conf_path)
-
-    editor.replace_data(f"cluster_name: {cluster_name}", f"cluster_name: {original_attr['cluster_name']}")
-
+    editor = utils.FileEdit(corosync_conf_path) 
+    editor.replace_data(f"cluster_name: {original_attr['cluster_name']}", f"cluster_name: {cluster_name}")
+    editor.replace_data(f"bindnetaddr: {original_attr['bindnetaddr']}", f"bindnetaddr: {bindnetaddr}")
+    interface_content = f'''
+        interface {{
+            ringnumber: 0
+            bindnetaddr: {bindnetaddr}
+            mcastport: 5405
+            ttl: 1
+        }}
+    '''
     if "interface" not in editor.data:
         editor.replace_data(f"crypto_hash: none", f"crypto_hash: none\n{interface_content}")
+
+    # editor.insert_data(interface, anchor=interface_pos, type='under')
+    
+    editor.insert_data(nodelist, anchor=nodelist_pos, type='above')
+    if len(bindnetaddr_list) > 1:
+        editor.insert_data(f'\trrp_mode: passive', anchor='        # also set rrp_mode.', type='under')
 
     # 移动 syslog_facility: daemon 和 timestamp: on 到 logging 部分
     if logging_content not in editor.data:
@@ -89,11 +75,15 @@ def change_corosync3_conf(cluster_name, nodelist, ssh_conn=None):
     if quorum_content not in editor.data:
         editor.replace_data(f"provider: corosync_votequorum", f"provider: corosync_votequorum\n{quorum_content}")
 
-    if 'nodelist' in editor.data:
-        editor.insert_data("        expected_votes: 2", anchor=quorum_pos, type='under')
-        editor.insert_data(nodelist, anchor=nodelist_pos, type='above')
+    utils.exec_cmd(f'echo "{editor.data}" > {corosync_conf_path}', ssh_conn)
 
-        utils.exec_cmd(f'echo "{editor.data}" > {corosync_conf_path}', ssh_conn)
-    else:
-        # Add new nodelist
-        editor.insert_data(nodelist, anchor=nodelist_pos, type='above')
+
+def change_corosync3_conf(cluster_name, nodelist, ssh_conn=None):
+    editor = utils.FileEdit(corosync_conf_path)
+
+    editor.replace_data(f"cluster_name: {original_attr['cluster_name']}", f"cluster_name: {cluster_name}")
+    editor.insert_data("        expected_votes: 2", anchor=quorum_pos, type='under')
+    editor.insert_data(nodelist, anchor=nodelist_pos, type='above')
+
+    utils.exec_cmd(f'echo "{editor.data}" > {corosync_conf_path}', ssh_conn)
+
